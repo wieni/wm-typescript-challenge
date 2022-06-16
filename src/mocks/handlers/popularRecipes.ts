8f49e4382b8893abe750cd3e9bf4f18ef7b4d7b0
Sander <sander@wieni.be>
2022-06-16 09:27:03 +0200

    🧹 Remove unused params variable

--- src/mocks/handlers/popularRecipes.ts
+++ src/mocks/handlers/popularRecipes.ts
@@ -5,10 +5,6 @@ import cocktails from "../data/cocktails.json";
 export const popularRecipes = rest.get(
   "/api/recipes/popular",
   (req, res, { json }) => {
-    const params = {
-      limit: (req.url.searchParams.get("limit") as string) || "5",
-    };
-
     return res(
       json({
         cocktails,
